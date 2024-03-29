4bddfdbd54ae35252f45a3ac8c181c94f6c4ea24
Raiane Campos Januário <raianejanu@hotmail.com>
2024-03-29 20:22:21 -0300

    feat: completa desafio

--- desafio.kt
+++ desafio.kt
@@ -2,20 +2,32 @@
 
 enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
 
-class Usuario
+data class Usuario(val matricula: Int, val nome: String)
 
 data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
-
+data class Formacao(val nome: String, val conteudos: List<ConteudoEducacional>) {
+    
+    private val nivel = Nivel.INTERMEDIARIO
+    
     val inscritos = mutableListOf<Usuario>()
     
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        inscritos.add(usuario)
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+  val conteudos = mutableListOf<ConteudoEducacional>()
+  conteudos.add(ConteudoEducacional("Entendendo DDD",10))
+  conteudos.add(ConteudoEducacional("Linguagem Ubíqua",20))
+  conteudos.add(ConteudoEducacional("Domínio e subdomínios",15))
+  conteudos.add(ConteudoEducacional("Contextos delimitados",30))
+  conteudos.add(ConteudoEducacional("Mapas de contextos",20))
+  conteudos.add(ConteudoEducacional("Design patterns",40))
+  
+  val formacao = Formacao("Arquitetura de software", conteudos)
+  
+  formacao.matricular(Usuario(1,"Davi"))
+  formacao.matricular(Usuario(2,"Ana"))
 }
